18e7a89fdcb87174eaa8d9b90580eb14912d56bf
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-03-16 04:31:24 +0700

    Update tourney.js

--- commands/tourney.js
+++ commands/tourney.js
@@ -3,7 +3,7 @@ module.exports = (client) => {
     module.cooldown = 3000;
 
     module.execute = function execute(channel, user, msg, IsSelf, args) {
-        client.say(channel, `Main Sheet: https://go.tawan475.dev/Tz9o50c4`);
+        client.say(channel, `Register: https://forms.gle/acBVMpc6oJrLfRhw6, Main Sheet: https://go.tawan475.dev/Tz9o50c4`);
     }
 
     return module;
