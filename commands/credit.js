715b8d670a2a4665d00f941af22d02c7bd0767d9
tawan475 <43491902+tawan475@users.noreply.github.com>
2022-05-06 09:50:09 +0700

    change username

--- commands/credit.js
+++ commands/credit.js
@@ -6,9 +6,9 @@ module.exports = (client) => {
         let toCheck = args[0] || user.username;
 
         let credit = await client.db.get("credit", toCheck);
-        if (!credit) return client.say(channel, `${user.username}, No data for this username.`);
-        if (credit.credit == 0) return client.say(channel, `${user.username}, You have 0 credits, send bits to get more credits! For more infomation: !help`);
-        return client.say(channel, `${user.username}, You have ${credit.credit} credits. For more infomation: !help`);
+        if (!credit) return client.say(channel, `${user.username}, No data for ${toCheck}.`);
+        if (credit.credit == 0) return client.say(channel, `${toCheck}, You have 0 credits, send bits to get more credits! For more infomation: !help`);
+        return client.say(channel, `${user.username}, ${toCheck} have ${credit.credit} credits. For more infomation: !help`);
     }
 
     return module;
